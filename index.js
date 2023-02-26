4f46104c32bf323be1a46a7c1c384a4ce631e2f0
hafeed <hafeedpallikkal@gmail.com>
2023-02-26 22:38:40 +0530

    some valuable update 😉

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 const express = require("express");
 var http = require("http");
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
-var io = require("socket.io")(server, {
-    cors:
-    {
-        origin: "*",
-    }
-});
+var io = require("socket.io")(server);
 
 //middleware
 app.use(express.json());
-app.use(cors());
-io.on("Connection", (socket)=>{
-    console.log("connected");
-})
+var clients = {};
 
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+  socket.on("signin", (id) => {
+    console.log(id);
+    clients[id] = socket;
+    console.log(clients);
+  });
+  socket.on("message", (msg) => {
+    console.log(msg);
+    let targetId = msg.targetId;
+    if (clients[targetId]) clients[targetId].emit("message", msg);
+  });
+});
 
-server.listen(port,()=>{
-    console.log("server Started");
-})
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
+});
