63b0f9ee8f2e7101a69826519689393ce27303ee
hafeed <hafeedpallikkal@gmail.com>
2023-02-27 12:04:07 +0530

    updation

--- index.js
+++ index.js
@@ -7,21 +7,14 @@ var io = require("socket.io")(server);
 
 //middleware
 app.use(express.json());
-var clients = {};
-
 io.on("connection", (socket) => {
-  console.log("connetetd");
-  console.log(socket.id, "has joined");
-  socket.on("signin", (id) => {
-    console.log(id);
-    clients[id] = socket;
-    console.log(clients);
-  });
-  socket.on("message", (msg) => {
+  console.log("connected");
+  console.log(socket.id,"has joined");
+  socket.on("/test",(msg)=>{
     console.log(msg);
-    let targetId = msg.targetId;
-    if (clients[targetId]) clients[targetId].emit("message", msg);
-  });
+  })
+
+
 });
 
 server.listen(port, "0.0.0.0", () => {
