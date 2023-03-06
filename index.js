8b625b34dbeda424e7997ab9ffddc416e646af9f
hafeed <hafeedpallikkal@gmail.com>
2023-03-07 00:02:58 +0530

    small update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 var http = require("http");
+const ms = require("ms");
 const app = express();
 const port = process.env.PORT || 5000;
 var server = http.createServer(app);
@@ -7,14 +8,27 @@ var io = require("socket.io")(server);
 
 //middleware
 app.use(express.json());
+var clients ={};
 io.on("connection", (socket) => {
   console.log("connected");
   console.log(socket.id,"has joined");
-  socket.on("/test",(msg)=>{
-    console.log(msg);
+  socket.on("signin",(id)=>{
+    console.log(id);
+    clients[id] =socket;
+    console.log(clients);
   })
 
+socket.on("message",(msg)=>{
+  console.log(msg);
+  console.log(msg.targetid);
+  let targetId = msg.targetid;
+ console.log(clients[targetId]);
+  if(clients[targetId]) {
+    console.log('emit success')
+    clients[targetId].emit("message",msg);
 
+}
+})
 });
 
 server.listen(port, "0.0.0.0", () => {
